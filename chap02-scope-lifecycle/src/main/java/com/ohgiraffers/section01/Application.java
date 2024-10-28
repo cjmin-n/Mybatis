675fd53ab0e5225e061a6c82c996a801b6b6abf2
jeongmin <mymemose@naver.com>
2024-10-28 11:15:58 +0900

    chap02

--- chap02-scope-lifecycle/src/main/java/com/ohgiraffers/section01/Application.java
+++ chap02-scope-lifecycle/src/main/java/com/ohgiraffers/section01/Application.java
@@ -1,8 +1,19 @@
 package com.ohgiraffers.section01;
 
+import static com.ohgiraffers.section01.Template.getSqlSession;
+
 public class Application {
 
     public static void main(String[] args) {
+
+        // 싱글톤 확인
+        System.out.println("session 1번 : " + getSqlSession().hashCode());
+        System.out.println("session 2번 : " + getSqlSession().hashCode());
+        /*sqlSessionFactory 의 hashCode : 1864350231
+        session 1번 : 727001376
+        sqlSessionFactory 의 hashCode : 1864350231
+        session 2번 : 660143728*/
+
 
     }
 }
